d8ebbd79f586a4a17976d5863d36f0223218e620
peter-rb2 <109708299+peter-rb2@users.noreply.github.com>
2023-02-22 10:20:57 +0800

    first commit

---
+++ tests/example.spec.js
@@ -0,0 +1,16 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+// @ts-check
+const { test, expect } = require("@playwright/test");
+test("login", async ({ page }) => {
+  await page.goto("http://farmholding-dev-fe.azurewebsites.net/");
+  await page.getByRole("button", { name: "profile" }).click();
+  await page.getByLabel("E-mailadres").click();
+  await page.getByLabel("E-mailadres").fill("peter@rb2.nl");
+  await page.getByLabel("Wachtwoord").click();
+  await page.getByLabel("Wachtwoord").fill("12345678");
+  await page.getByRole("button", { name: "Inloggen", exact: true }).click();
+  await page.getByRole("button", { name: "profile" }).click();
+  await expect(page).toHaveTitle(/Persoonlijke gegevens/i);
+});
